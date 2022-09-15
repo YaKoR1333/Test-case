2ee957e9aaf32acc458b88c794722cdab537f670
Daniil RodionoV <daniil.rodionoff@mail.ru>
2022-09-15 13:41:51 +0300

    Замечания:  - не очень хорошо иметь while True: в коде без чёткого условия выхода из цикла, - nv.listen луше использовать несколько раз - проверка с построением кортежа результатов так же интересна, но не решает вопрос приоритетности (что если две сущности сработали??) - проверять наличие записей норм, но не в течении диалога (зачем нам выдавать себя и говорить синтезом, если можно заранее проверить и если что дозалить фразы)
    Исправления:
    - Теперь цикл прекращается как только очередь будет пустая
    - Сделал несколько nv.listen для распознавания только сущностей нужных на данном этапе диалога, не уверен что это поможет избавиться от всех случаев где могут сработать 2 сущности
    - Теперь проверяю наличие записей до начала диалога

--- Neuro.net/recommendation_skript.py
+++ Neuro.net/recommendation_skript.py
@@ -7,7 +7,6 @@ import queue
 import dialog_tree
 import dialog_answers
 
-
 nn = NeuroNetLibrary(nlu_call, event_loop)
 nlu = NeuroNluLibrary(nlu_call, event_loop)
 nv = NeuroVoiceLibrary(nlu_call, loop)
@@ -17,6 +16,20 @@ beg_dialog = 'hello'
 
 q.put(beg_dialog)
 visited_branch = set()
+
+nv.media_params({'asr': 'google', 'tts': 'google', 'lang': 'ru_RU'})
+
+
+def hello_logic_entity_list() -> list[str]:
+    """возвращает список сущностей для распознавания в hello_logic"""
+
+    return ['confirm', 'wrong_time', 'repeat']
+
+
+def main_logic_entity_list() -> list[str]:
+    """возвращает список сущностей для распознавания в main_logic"""
+
+    return ['recommendation_score', 'recommendation', 'question', 'wrong_time', 'repeat']
 
 
 def _clear_queue():
@@ -35,25 +48,32 @@ def _add_node_in_queue(node: str):
         q.put(node)
 
 
-def _check_recommend_main_in_visited_branch():
+def _check_recommend_main_in_visited_branch() -> bool:
     """проверяет посещение диалогового узла recommend_main"""
 
     return 'recommend_main' in visited_branch
 
 
-def _check_len_visited_branch():
+def _check_len_visited_branch() -> bool:
     """проверка на то, что ни один диалоговый узел не был посещён"""
 
     return len(visited_branch) == 0
 
 
-def _check_len_message(data: str):
+def _check_len_message(data: str) -> bool:
     """проверка на то сказано ли хоть что-то"""
 
     return len(data) > 0
 
 
-def _dialog_node(node: str):
+def _check_dialog_has_record(prompt_lit: list[str]):
+    """проверяет существования аудио файлов для диалога, если файла нет дозаливаем его"""
+    for prompt in prompt_lit:
+        if nv.has_record(prompt) is False:
+            pass
+
+
+def _dialog_node(node: str, next_logic_step: str):
     """вызывает проигрывание аудио файла промпта и добавляет возможные узлы в очередь"""
 
     recommend_null_count = nn.counter('recommend_null_count')
@@ -68,73 +88,99 @@ def _dialog_node(node: str):
         recommend_null_count = nn.counter('recommend_null_count', '+')
 
     if recommend_null_count == 2 or recommend_default_count == 2 or hello_null_count == 2:
-        _dialog_end('hangup_null')
+        _dialog_end('hangup_null', 'dialogue_outcome')
     else:
-        if nv.has_record(node):
-            nv.say(node)
-            _add_node_in_queue(node)
-        else:
-            nv.synthesize(dialog_answers.dialog_answers(node))
-            _add_node_in_queue(node)
+        nv.say(node)
+        _add_node_in_queue(node)
 
-    return main()
+    return exec(next_logic_step + '()')
 
 
-def _dialog_end(node: str):
+def _dialog_end(node: str, dialog_outcome: str):
     """вызывает проигрывание аудио файла промпта и завершает диалог"""
 
-    if nv.has_record(node):
-        nv.say(node)
-        _dialog_done(node)
-    else:
-        nv.synthesize(dialog_answers.dialog_answers(node)[0])
-        _dialog_done(node)
+    nv.say(node)
+    _dialog_done(node, dialog_outcome)
 
 
-def _dialog_done(node: str):
+def _dialog_done(node: str, dialog_outcome: str):
     """заносит результат диалога в логи, отчищает очередь и завершает диалог"""
 
-    nn.log('dialogue_outcome', dialog_answers.dialog_answers(node)[1])
+    nn.log(dialog_outcome, dialog_answers.dialog_answers(node)[1])
     _clear_queue()
     nn.dialog.result = nn.RESULT_DONE
 
 
+def _recognizer(data: str, r, next_logic_step: str):
+    """определяет какой должен быть ответ от робота"""
+
+    while q.qsize() > 0:
+
+        get_node = q.get()
+
+        check_list = (r.result.entity('confirm'), r.result.entity('wrong_time'), r.result.entity('repeat'),
+                      tuple(r.result.entity('recommendation_score')), r.result.entity('recommendation'),
+                      r.result.entity('question'), get_node, _check_len_message(data), _check_len_visited_branch(),
+                      _check_recommend_main_in_visited_branch())
+        check_dict = {
+            (None, None, None, None, None, None, 'hello', False, True, False): _dialog_node,
+            (None, None, None, None, None, None, 'hello_null', False, False, False): _dialog_node,
+            (None, None, True, None, None, None, 'hello_repeat', True, False, False): _dialog_node,
+            (True, None, None, None, None, None, 'recommend_main', True, False, False): _dialog_node,
+            (None, None, None, None, None, None, 'recommend_main', True, False, False): _dialog_node,
+            (None, None, None, None, None, None, 'recommend_null', False, False, True): _dialog_node,
+            (None, None, None, None, 'negative', None, 'recommend_score_negative', True, False, True): _dialog_node,
+            (None, None, None, None, 'neutral', None, 'recommend_score_neutral', True, False, True): _dialog_node,
+            (None, None, None, None, 'positive', None, 'recommend_score_positive', True, False, True): _dialog_node,
+            (None, None, True, None, None, None, 'recommend_repeat', True, False, True): _dialog_node,
+            (None, None, None, None, 'dont_know', None, 'recommend_repeat_2', True, False, True): _dialog_node,
+            (None, None, None, None, None, None, 'recommend_default', True, False, True): _dialog_node,
+            (None, True, None, None, None, None, 'hangup_wrong_time', True, False, True | False): _dialog_end,
+            (None, None, None, tuple(range(9)), None, None, 'hangup_negative', True, False, True): _dialog_end,
+            (None, None, None, tuple(range(9, 11)), None, None, 'hangup_positive', True, False, True): _dialog_end,
+            (None, None, None, None, None, True, 'forward', True, False, True): _dialog_end,
+        }
+        if check_list in check_dict:
+            return check_dict[check_list](get_node, next_logic_step)
+        else:
+            continue
+
+
+def hello_main():
+    nn.log('unit', 'hello_main')
+    with nv.listen(entities=hello_logic_entity_list()) as r:
+        data = r.result.utterance()
+        _recognizer(data, r, 'hello_logic')
+
+
+def hello_logic():
+    with nv.listen(entities=hello_logic_entity_list()) as r:
+        data = r.result.utterance()
+        if r.result.entity('confirm') or (r.result.has_entities() is False and data):
+            _recognizer(data, r, 'main_logic')
+        elif (r.result.entity('confirm') is False) or r.result.entity('wrong_time'):
+            _recognizer(data, r, 'dialogue_outcome')
+        else:
+            _recognizer(data, r, 'hello_logic')
+
+
+def main_logic():
+    with nv.listen(entities=main_logic_entity_list()) as r:
+        data = r.result.utterance()
+        if r.result.has_entity('recommendation_score')\
+                or r.result.entity('wrong_time')\
+                or r.result.entity('question'):
+            _recognizer(data, r, 'dialogue_outcome')
+        else:
+            _recognizer(data, r, 'main_logic')
+
+
 def main():
-
-    with nv.listen(entities='confirm, wrong_time, repeat, recommendation_score, recommendation, question') as r:
-
-        nv.media_params({'asr': 'google', 'tts': 'google', 'lang': 'ru_RU'})
-
-        while True:
-            get_node = q.get()
-            data = r.result.utterance()
-            check_list = (r.result.entity('confirm'), r.result.entity('wrong_time'), r.result.entity('repeat'),
-                          tuple(r.result.entity('recommendation_score')), r.result.entity('recommendation'),
-                          r.result.entity('question'), get_node, _check_len_message(data), _check_len_visited_branch(),
-                          _check_recommend_main_in_visited_branch())
-            check_dict = {
-                (None, None, None, None, None, None, 'hello', False, True, False): _dialog_node,
-                (None, None, None, None, None, None, 'hello_null', False, False, False): _dialog_node,
-                (None, None, True, None, None, None, 'hello_repeat', True, False, False): _dialog_node,
-                (True, None, None, None, None, None, 'recommend_main', True, False, False): _dialog_node,
-                (None, None, None, None, None, None, 'recommend_main', True, False, False): _dialog_node,
-                (None, None, None, None, None, None, 'recommend_null', False, False, True): _dialog_node,
-                (None, None, None, None, 'negative', None, 'recommend_score_negative', True, False, True): _dialog_node,
-                (None, None, None, None, 'neutral', None, 'recommend_score_neutral', True, False, True): _dialog_node,
-                (None, None, None, None, 'positive', None, 'recommend_score_positive', True, False, True): _dialog_node,
-                (None, None, True, None, None, None, 'recommend_repeat', True, False, True): _dialog_node,
-                (None, None, None, None, 'dont_know', None, 'recommend_repeat_2', True, False, True): _dialog_node,
-                (None, None, None, None, None, None, 'recommend_default', True, False, True): _dialog_node,
-                (None, True, None, None, None, None, 'hangup_wrong_time', True, False, True | False): _dialog_end,
-                (None, None, None, tuple(range(9)), None, None, 'hangup_negative', True, False, True): _dialog_end,
-                (None, None, None, tuple(range(9, 11)), None, None, 'hangup_positive', True, False, True): _dialog_end,
-                (None, None, None, None, None, True, 'forward', True, False, True): _dialog_end,
-
-            }
-            if check_list in check_dict:
-                exec(check_dict[check_list](get_node))
-            else:
-                continue
+    _check_dialog_has_record(dialog_tree.recommendation_dialog_tree.keys())
+    try:
+        hello_main()
+    except InvalidCallStateError:
+        nn.log("Call ended")
 
 
 if __name__ == '__main__':
